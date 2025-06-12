6f3f07715cde79873134d70778f4896ba49a24b2
Khayot Razzakov <hrazzoqov0002@gmail.com>
2025-06-12 07:56:58 +0500

    created validate_file and validate_duration (SongSerializer)

--- main/serializers.py
+++ main/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer, ValidationError
+from datetime import timedelta
 from .models import *
 
 class SingerSerializer(ModelSerializer):
@@ -21,4 +22,15 @@ class SongSerializer(ModelSerializer):
     class Meta:
         model = Song
         fields = '__all__'
+
+    def validate_file(self, file):
+        if not str(file).lower().endswith('.mp3'):
+            raise ValidationError('Only mp3 files are allowed')
+        return file
+
+    def validate_duration(self, duration):
+        max_duration = timedelta(hours=0, minutes=7)
+        if duration and duration > max_duration:
+            raise ValidationError('Duration is too long, need 00:07:00 minutes')
+        return duration
 
